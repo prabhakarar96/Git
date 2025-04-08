76a4bc24abfc168301bdd00caf6e14db38bcf510
rakshith <rakshith.phs@gmail.com>
2025-04-08 01:01:31 -0500

    delete the project

--- learn-spring/src/main/java/com/in28minutes/learn_spring/AppGamingBasic.java
+++
@@ -1,9 +0,0 @@
-package learn-spring.src.main.java.com.in28minutes.learn_spring;
-
-public class AppGamingBansic {
-public static void main(String[] args) {
-    var marioGame= new MarioGame();  
-    var gameRunner = new GameRunner(marioGame);
-    gameRunner.run();
-    }
-}
