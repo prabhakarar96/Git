f0c2febe2fdcd3fffd8180569824588b73381484
rakshith <rakshith.phs@gmail.com>
2025-04-08 01:15:37 -0500

    added new 3 java files

---
+++ learn-spring/src/main/java/com/in28minutes/learn_spring/AppGamingBasic.java
@@ -0,0 +1,12 @@
+package com.in28minutes.learn_spring;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+
+public class AppGamingBasic {
+public static void main(String[] args) {
+    var marioGame = new MarioGame();
+    var gameRunner = new GameRunner(marioGame);
+    gameRunner.run();
+}
+}
