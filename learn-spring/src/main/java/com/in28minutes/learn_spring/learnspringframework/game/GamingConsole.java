554378426be59c8c7ba74b8b2125128ab357250e
rakshith <rakshith.phs@gmail.com>
2025-04-08 01:58:53 -0500

    Updated loosely coupled codes

---
+++ learn-spring/src/main/java/com/in28minutes/learn_spring/learnspringframework/game/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.in28minutes.learn_spring.learnspringframework.game;
+
+public interface GamingConsole {
+void up();
+void down();
+void left();
+void right();
+}
--- learn-spring/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
+++ learn-spring/src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -2,18 +2,18 @@ package com.in28minutes.learnspringframework.game;
 
 public class MarioGame {
 public void up() {
-        System.out.println("Jump");
-    }
+    System.out.println("Jump");
+}
 
 public void down() {
-        System.out.println("Go into a hole");
-    }
+    System.out.println("Go into a hole");
+}
 
 public void left() {
-        System.out.println("Go back");
-    }
+    System.out.println("Go back");
+}
 
 public void right() {
-        System.out.println("Go forward");
+    System.out.println("Go forward");
 }
 }
