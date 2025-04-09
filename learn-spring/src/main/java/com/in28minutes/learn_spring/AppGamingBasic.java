7d510863150de9108600eb8bfe16ca8cf2199b40
rakshith <rakshith.phs@gmail.com>
2025-04-09 00:14:41 -0500

    Added Pacman.java

--- learn-spring/src/main/java/com/in28minutes/learn_spring/AppGamingBasic.java
+++ learn-spring/src/main/java/com/in28minutes/learn_spring/AppGamingBasic.java
@@ -5,11 +5,12 @@ import com.in28minutes.learn_spring.learnspringframework.game.GameRunner;
 import com.in28minutes.learn_spring.learnspringframework.game.MarioGame;
 import com.in28minutes.learn_spring.learnspringframework.game.SuperContraGame;  
 import com.in28minutes.learn_spring.learnspringframework.game.GamingConsole;
-
+import com.in28minutes.learn_spring.learnspringframework.game.Pacman;
 public class AppGamingBasic {
 public static void main(String[] args) {
     //var game = new MarioGame();
-    var game = new SuperContraGame();
+    //var game = new SuperContraGame();
+    var game = new Pacman();
     var gameRunner = new GameRunner(game);
     gameRunner.run();
 }
---
+++ learn-spring/src/main/java/com/in28minutes/learn_spring/learnspringframework/game/Pacman.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learn_spring.learnspringframework.game;
+
+public class Pacman implements GamingConsole {
+    public void up() {
+        System.out.println("Go up");
+    }
+
+    public void down() {
+        System.out.println("Go down");
+    }
+
+    public void left() {
+        System.out.println("Go left");
+    }
+
+    public void right() {
+        System.out.println("Go right");
+    }
+    
+}
